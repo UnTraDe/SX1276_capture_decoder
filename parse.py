4ac6853b899aaf4f3749287eda30c2d39c7ee45a
Tomer Abramovich <untradeta@gmail.com>
2020-01-15 23:05:43 +0200

    because of a timestamp rounding issue with exported csv from DSView, avoid constructing SPI frames manually, and use '1-SPI' decoder instead

--- parse.py
+++ parse.py
@@ -1,14 +1,19 @@
 import csv
+import sys
+import numpy as np
 
 # files
 
-FILE_MISO = "caps/MISO.csv"
-FILE_MOSI = "caps/MOSI.csv"
-FILE_RAW = "caps/raw.csv"
+#FOLDER = "running_moving_ch1"
+#FOLDER = "running_idle"
+#FOLDER = "running_ch5_switch_low"
+#FOLDER = "running_ch5_switch_high"
+#FOLDER = "running_th_-13"
+#FOLDER = "running_th_29"
+FOLDER = "binding_rx_off"
 
-# capture indices
-
-FILE_RAW_CS_INDEX = 1
+FILE_MISO = "caps/" + FOLDER + "/MISO.csv"
+FILE_MOSI = "caps/" + FOLDER + "/MOSI.csv"
 
 # timeline indices
 
@@ -26,91 +31,54 @@ def insert_after(timeline, timestamp, value):
 			break
 
 if __name__ == "__main__":
+
+	if len(sys.argv) == 3:
+		FILE_MISO = sys.argv[1]
+		FILE_MOSI = sys.argv[2]
+	elif len(sys.argv) == 2:
+		FOLDER = sys.argv[1]
+		FILE_MISO = FOLDER + "/MISO.csv"
+		FILE_MOSI = FOLDER + "/MOSI.csv"
+
 	timeline = []
 
 	# create timeline from MISO file
 
 	with open(FILE_MISO) as f:
+		next(f)
 		for row in csv.reader(f, delimiter=','):
 			timestamp = float(row[1])
-			timeline.append([timestamp, int(row[2], 16)])
+			
+			arr = []
+
+			for b in row[2].split(" "):
+				arr.append(int(b, 16))
+
+			timeline.append([timestamp, arr])
 
 	# merge MOSI into timeline
 
 	i = 0
 
 	with open(FILE_MOSI) as f:
+		next(f)
 		for row in csv.reader(f, delimiter=','):
 			timestamp = float(row[1])
 			
 			if timeline[i][TIMESTAMP] != timestamp:
 				raise Exception("timestamp mismatch, make sure all the input files are from the same capture!")
 			
-			timeline[i].append(int(row[2], 16))
+			arr = []
+
+			for b in row[2].split(" "):
+				arr.append(int(b, 16))
+
+			timeline[i].append(arr)
 			i = i + 1
-
-	# add frame start / frame end markers (determined by CS line)
-
-	prev = 1
-
-	with open(FILE_RAW) as f:
-		i = 0
-		first_idle_found = False
-
-		for row in csv.reader(f, delimiter=','):
-			# skip first 5 rows
-			if i < 5:
-				i = i + 1
-				continue
-
-			timestamp = float(row[0])
-			value = int(row[FILE_RAW_CS_INDEX])
-
-			# if CS is low before first_idle_found we are in the middle of the frame,
-			# so skip ahead until this partial frame is ended
-			if not first_idle_found:
-				if value == 1:
-					first_idle_found = True
-					print(f"first idle CS sample found at {timestamp}")
-				else:
-					continue
-
-			if value == 0 and prev == 1: # high > low transition
-				insert_after(timeline, timestamp, [timestamp, 0, "frame_start"])
-			elif value == 1 and prev == 0: # low > high transition
-				insert_after(timeline, timestamp, [timestamp, 0, "frame_end"])
-
-			prev = value
 
 	with open("timeline.txt", "w") as f:
 		for s in timeline:
-			f.write(str(s) + "\n")
-
-	# seperate the timeline into data frames
-
-	data = []
-	frame = []
-	in_frame = False
-
-	for sample in timeline:
-		if in_frame:
-			if type(sample[FRAME_MARKER]) == str and sample[FRAME_MARKER] == "frame_end":
-				data.append(frame.copy())
-				frame.clear()
-				in_frame = False
-			elif type(sample[FRAME_MARKER]) == str and sample[FRAME_MARKER] == "frame_start":
-				raise Exception("frame_start while in frame")
-			else:
-				frame.append(sample)
-		else:
-			if type(sample[FRAME_MARKER]) == str and sample[FRAME_MARKER] == "frame_start":
-				in_frame = True
-			else:
-				raise Exception("invalid row while outside of frame")
-
-	# uncomment this if you want to see the last frame even if it's truncated
-	# if in_frame: # ended mid frame
-	# 	data.append(frame.copy())
+			f.write(f"{s[0]}, {s[1]}, {s[2]}\n")
 
 	# read register map
 
@@ -125,23 +93,90 @@ if __name__ == "__main__":
 
 	# generate user friendly text
 
-	for frame in data:
-		# first MOSI byte of the frame is the register address and it's MSB indicates read/write (1 = write, 0 = read)
-		is_writing = (frame[0][MOSI] & 0x80)
-		target_register = frame[0][MOSI] & 0x7F
+	with open("output.txt", "w") as f:
+		for frame in timeline:
+			# first MOSI byte of the frame is the register address and it's MSB indicates read/write (1 = write, 0 = read)
+			is_writing = (frame[MOSI][0] & 0x80)
+			target_register = frame[MOSI][0] & 0x7F
 
-		if target_register in reg_map:
-			reg_name = reg_map[target_register]
-		else:
-			reg_name = ""
-		
-		read_or_write = "write" if is_writing else "read"
+			if target_register in reg_map:
+				reg_name = reg_map[target_register]
+			else:
+				reg_name = ""
+			
+			read_or_write = "write" if is_writing else "read"
+			payload = frame[MOSI][1:] if is_writing else frame[MISO][1:]
 
-		print(f"{read_or_write} ({len(frame) - 1} bytes) reg 0x{target_register:02x} ({reg_name}):")
+			f.write(f"{read_or_write} ({len(payload)} bytes) reg 0x{target_register:02x} ({reg_name}):\n")
 
-		payload = "\t"
+			output = "\t"
+			
+			for b in payload:
+				output += f"0x{b:02x} "
+				
+			f.write(output + "\n")
 
-		for t in frame[1:]:
-			payload += "0x{:02x} ".format(t[MOSI] if is_writing else t[MISO])
+	# find frequency hopping map
 
-		print(payload)
+	# frequency synthesizer step (Hz)
+	fstep = 61
+
+	first = True
+	first_timestamp = 0
+
+	for frame in timeline:
+		is_writing = (frame[MOSI][0] & 0x80)
+		target_register = frame[MOSI][0] & 0x7F
+
+		if is_writing and target_register == 0x06:
+			if first:
+				first = False
+				first_timestamp = frame[TIMESTAMP]
+
+			t = frame[TIMESTAMP] - first_timestamp
+			freq = (frame[MOSI][1] << 16 | frame[MOSI][2] << 8 | frame[MOSI][3]) * fstep
+
+			#print(f"{t}: {freq}")
+			print(f"{freq}")
+
+	# extract channel data
+
+	for frame in timeline:
+		is_writing = (frame[MOSI][0] & 0x80)
+		target_register = frame[MOSI][0] & 0x7F
+
+		bs = []
+
+		if is_writing and target_register == 0x00:
+
+			for b in frame[MOSI][1:]:
+				bs.append(b)
+
+			chs = bs[8:]
+			#print(chs)
+			# ch = frame[9][MOSI] << 8 | frame[10][MOSI]
+
+			ch1 = chs[0] << 3 | ((chs[1] >> 5) & 0b111)
+			ch2 = (chs[1] & 0b11111) << 6 | ((chs[2] >> 2) & 0b111111)
+			ch5 = (chs[5] & 0b1111) << 4 | ((chs[6] >> 1) & 0b1111111)
+			
+			string = ""
+
+			for c in bs[4:]:
+				string += format(c, "08b") + " "
+
+			print(bs[3])
+
+			#print(chs[0])
+			#print(np.array([ch1, ch5]))
+
+			#print(np.array(chs))
+			#print(frame[9][MOSI])
+
+			#pkt_timestamp = frame[1][MOSI] << 24 | frame[2][MOSI] << 16 | frame[3][MOSI] << 8 | frame[4][MOSI]
+			#pkt_val = frame[5][MOSI] << 24 | frame[6][MOSI] << 16 | frame[7][MOSI] << 8 | frame[8][MOSI]
+
+			#print(frame[4][MOSI])
+			#print(bs[:3])
+
+
